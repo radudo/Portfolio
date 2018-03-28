4bbd6868a5aebeb4a0ea380cb56da70d84748fef
MSI\eu_ra <eu_radu2000@yahoo.com>
2018-03-29 00:18:00 +0100

    First slide done menu 90%

--- Portfolio_Up/Scripts/initializer.js
+++ Portfolio_Up/Scripts/initializer.js
@@ -16,6 +16,12 @@
     $.fn.fullpage.moveSectionDown();
     });
 
+    $(document).ready(function () {
+        $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function () {
+            $(this).toggleClass('open');
+        });
+    });
+
 
 
 });
