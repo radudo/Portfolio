fe78e89b02a937430ef25a80499e55b7805e1c66
MSI\eu_ra <eu_radu2000@yahoo.com>
2018-03-28 22:27:54 +0100

    Creating the desired layout (Modal menu and first page)

---
+++ Portfolio_Up/Scripts/initializer.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    $('#fullpage').fullpage({
+        //Menu
+        anchors: ['firstPage', 'secondPage', 'thirdPage'],
+        menu: '#myMenu',
+        navigation: true,
+        navigationPosition: 'right',
+        navigationTooltips: ['First page', 'Second page', 'Third and last page'],
+        //Sections
+        sectionsColor: ['#1bbc9b', '#4BBFC3'],
+
+
+    });
+
+    $(document).on('click', '#movedown', function () {
+    $.fn.fullpage.moveSectionDown();
+    });
+
+
+
+});
+
+
