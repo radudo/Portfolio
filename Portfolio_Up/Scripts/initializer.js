e72771e4a3f6e44307e8544e2465c372dc6a66a3
MSI\eu_ra <eu_radu2000@yahoo.com>
2018-03-31 00:24:42 +0100

    Second Page completed Æ Fully working scripts and animations for each category

--- Portfolio_Up/Scripts/initializer.js
+++ Portfolio_Up/Scripts/initializer.js
@@ -7,14 +7,49 @@
         navigationPosition: 'right',
         navigationTooltips: ['Intro', 'About', 'Contact'],
         //Sections
-        sectionsColor: ['#1bbc9b', '#4BBFC3','orange'],
+        sectionsColor: ['#1bbc9b', '#2c3e50','orange'],
         fitToSection: true,
         dragAndMove: true,
         recordHistory: true,
+        setLocAnchors: true,
 
+        afterLoad: function (anchorLink, index) {
+            var loadedSection = $(this);
+            //using anchorLink
+            if (anchorLink === 'secondPage') {
+                jQuery('.skillbar').each(function () {
+                    jQuery(this).find('.skillbar-bar').animate({
+                        width: jQuery(this).attr('data-percent')
+                    }, 2000);
+                });
+            }
+            //$('#fade').addClass('uk-animation-fade');
+        },
+        onLeave: function (index, nextIndex, direction) {
+            if (nextIndex === 2) {
+                $('#fade').addClass('uk-animation-fade');
+                $('#about').addClass('uk-animation-slide-right');
+               
 
+            } else if (direction === 'up' || direction === 'down') {
+                $('#fade').addClass('uk-animation-reverse');
+                $('#about').addClass('uk-animation-reverse');
+            }
+
+            if (index === 2) {
+                $('#fade').removeClass('uk-animation-fade uk-animation-reverse');
+                $('#about').removeClass('uk-animation-slide-right uk-animation-reverse');
+            }
+
+        },
        
+    
 
+    });
+
+
+    UIkit.util.on('#secondPage', 'show', function () {
+        // do something
     });
 
     $(document).on('click', '#movedown', function () {
@@ -56,9 +91,9 @@
 
     });
 
+  //Chart
   
-
-
+  
 
 });
 
